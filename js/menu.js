f82a111692973244a190256c715d9d494e2d816a
Pavlo1508 <130464415+Pavlo1508@users.noreply.github.com>
2023-10-03 08:50:36 +0200

    Started and ended homework 6. v.3

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+document.getElementById("menu-btn").addEventListener("click", function () {
+document.getElementById("menu").classList.add("is-open")
+})
+
+document.getElementById("menu-close").addEventListener("click", function () {
+document.getElementById("menu").classList.remove("is-open")
+})
--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,7 @@
-document.getElementById("modal-open-btn").addEventListener("click", function () {
-	document.getElementById("modal-order").classList.add("is-open")
+document.getElementById("modal-btn").addEventListener("click", function () {
+	document.getElementById("modal").classList.add("is-open")
 })
 
-document.getElementById("modal-close-order-btn").addEventListener("click", function () {
-	document.getElementById("modal-order").classList.remove("is-open")
+document.getElementById("modal-close-btn").addEventListener("click", function () {
+	document.getElementById("modal").classList.remove("is-open")
 })
